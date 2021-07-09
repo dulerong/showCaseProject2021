beefcd43bc97439cc6e8bd2fefb37a0cefdc2107
Le-Jung Du <lejung@qualias.jp>
2021-07-09 16:46:03 +0900

    updated routes

--- resources/js/router/constantRoutes.js
+++ resources/js/router/constantRoutes.js
@@ -13,83 +13,17 @@ export const routePath = {
     name: 'search',
     icon: 'mdi-magnify'
   },
-  register: {
-    title: null,
-    path: '/register',
-    name: 'register',
-    icon: null
-  },
-  edit: {
-    title: null,
-    path: '/edit',
-    name: 'edit',
-    icon: null
-  },
   manage: {
     title: 'Manage',
     path: '/manage',
     name: 'manage',
     icon: 'mdi-alpha-m-circle'
   },
-  inquiry: {
-    title: null,
-    path: '/inquiry/:id',
-    name: 'inquiry',
-    icon: null
-  },
-  calendar: {
-    title: 'イベントカレンダー',
-    path: '/calendar', 
-    name: 'calendar',
-    icon: 'calendar'
-  },
-  insidesales: {
-    title: 'インサイドセールス管理',
-    path: '/inside_sales',
-    name: 'insideSales',
-    icon: 'insideSales'
-  },
-  nosales: {
-    title: '担当未設定顧客管理',
-    path: '/no_sales',
-    name: 'noSales',
-    icon: 'insideSales'
-  },
-  statpersonal: {
-    title: '個人集計',
-    path: '/stat_personal',
-    name: 'statPersonal',
-    icon: 'statPersonal'
-  },
-  statbranch: {
-    title: '店舗集計',
-    path: '/stat_branch',
-    name: 'statBranch',
-    icon: 'statStore'
-  },
-  statbranchemployee: {
-    title: '店舗集計(担当者別)',
-    path: '/stat_branch_employee',
-    name: 'statBranchEmployee',
-    icon: 'statStore'
-  },
-  statInsideAll: {
-    title: '全店インサイド集計',
-    path: '/stat_inside_all',
-    name: 'statInsideAll',
-    icon: 'statInsideSales'
-  },
-  statInsideBranch: {
-    title: '店舗別インサイド集計',
-    path: '/stat_inside_branch',
-    name: 'statInsideBranch',
-    icon: 'statInsideSales'
-  },
-  sales: {
-    title: null,
-    path: '/sales/:id',
-    name: 'sales',
-    icon: null
+  stat: {
+    title: 'Statistics',
+    path: '/stat',
+    name: 'stat',
+    icon: 'mdi-file-chart'
   },
   login: {
     title: 'LOGIN',
@@ -125,8 +59,7 @@ export const navigation = [
       routePath.home,
       routePath.search,
       routePath.manage,
-      // routePath.insidesales,
-      // routePath.nosales
+      routePath.stat,
     ]
   },
   // {
@@ -137,21 +70,6 @@ export const navigation = [
   //     routePath.statbranchemployee,
   //     routePath.statInsideAll,
   //     routePath.statInsideBranch,
-  //     // {
-  //     //   title: '集客力グラフ',
-  //     //   path: '/storedata',
-  //     //   icon: 'stat'
-  //     // },
-  //     // {
-  //     //   title: '店舗別契約数及び契約率',
-  //     //   path: '/storedata',
-  //     //   icon: 'stat'
-  //     // },
-  //     // {
-  //     //   title: '媒体別販促・単価集計',
-  //     //   path: '/storedata',
-  //     //   icon: 'statMedia'
-  //     // },
   //   ]
   // },
   // {
--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -8,6 +8,7 @@ import LoginLayout from '~layouts/login'
 // pages
 import HomePage from '~pages/HomePage'
 import LoginPage from '~pages/LoginPage'
+import StatPage from '~pages/StatPage'
 import NotFoundPage from '~pages/NotFoundPage'
 
 // import { store } from './store'
@@ -20,116 +21,22 @@ export default [
     meta: { layout: DefaultLayout }
   },
   {
+    path: routePath.stat.path,
+    name: routePath.stat.name,
+    component: StatPage,
+    meta: { layout: DefaultLayout }
+  },
+  {
     path: routePath.login.path,
     name: routePath.login.name,
     component: LoginPage,
     meta: { layout: LoginLayout }
   },
   // { 
-  //   path: routePath.home.path, 
-  //   name: routePath.home.name, 
-  //   component: HomePage, 
-  //   meta: { layout:  DefaultLayout, requiresAuth: true } 
-  // },
-  // { 
-  //   path: routePath.search.path, 
-  //   name: routePath.search.name, 
-  //   component: CustomerSearch, 
-  //   meta: { layout:  DefaultLayout, requiresAuth: true },
-  // },
-  // {
-  //   path: routePath.register.path,
-  //   name: routePath.register.name,
-  //   component: CustomerCreate,
-  //   meta: { layout: DefaultLayout, requiresAuth: true }
-  // },
-  // {
-  //   path: routePath.edit.path,
-  //   name: routePath.edit.name,
-  //   component: CustomerEdit,
-  //   meta: { layout: DefaultLayout, requiresAuth: true },
-  //   beforeEnter: (to, from, next) => {
-  //     if (store.getters['customerInquiry/isInfoAvailable']) next()
-  //     else next({ name: routePath.search.name })
-  //   }
-  // },
-  // {
-  //   path: routePath.manage.path,
-  //   name: routePath.manage.name,
-  //   component: CustomerManage,
-  //   meta: { layout: DefaultLayout, requiresAuth: true }
-  // },
-  // {
-  //   path: routePath.inquiry.path,
-  //   name: routePath.inquiry.name,
-  //   component: CustomerInquiry,
-  //   meta: { layout: DefaultLayout, requiresAuth: true },
-  // },
-  // { 
-  //   path: routePath.calendar.path, 
-  //   name: routePath.calendar.name, 
-  //   component: CalendarPage, 
-  //   meta: { layout: DefaultLayout, requiresAuth: true } 
-  // },
-  // { 
-  //   path: routePath.insidesales.path, 
-  //   name: routePath.insidesales.name, 
-  //   component: InsideSales, 
-  //   meta: { layout: DefaultLayout, requiresAuth: true } 
-  // },
-  // { 
-  //   path: routePath.nosales.path, 
-  //   name: routePath.nosales.name, 
-  //   component: CustomerNoSales, 
-  //   meta: { layout: DefaultLayout, requiresAuth: true } 
-  // },
-  // {
-  //   path: routePath.statpersonal.path,
-  //   name: routePath.statpersonal.name,
-  //   component: StatPersonalPage,
-  //   meta: { layout: DefaultLayout, requiresAuth: true }
-  // },
-  // {
-  //   path: routePath.statbranch.path,
-  //   name: routePath.statbranch.name,
-  //   component: StatBranchPage,
-  //   meta: { layout: DefaultLayout, requiresAuth: true }
-  // },
-  // {
-  //   path: routePath.statbranchemployee.path,
-  //   name: routePath.statbranchemployee.name,
-  //   component: StatBranchEmployeePage,
-  //   meta: { layout: DefaultLayout, requiresAuth: true }
-  // },
-  // {
-  //   path: routePath.statInsideAll.path,
-  //   name: routePath.statInsideAll.name,
-  //   component: StatInsideAllPage,
-  //   meta: { layout: DefaultLayout, requiresAuth: true }
-  // },
-  // {
-  //   path: routePath.statInsideBranch.path,
-  //   name: routePath.statInsideBranch.name,
-  //   component: StatInsideBranchPage,
-  //   meta: { layout: DefaultLayout, requiresAuth: true }
-  // },
-  // {
-  //   path: routePath.sales.path,
-  //   name: routePath.sales.name,
-  //   component: SalesPage,
-  //   meta: { layout: SalesLayout, requiresAuth: true },
-  // },
-  // { 
   //   path: routePath.memberPasswordReset.path, 
   //   name: routePath.memberPasswordReset.name, 
   //   component: MemberPasswordResetPage, 
   //   meta: { layout:  DefaultLayout, requiresAuth: true } 
-  // },
-  // { 
-  //   path: routePath.login.path, 
-  //   name: routePath.login.name, 
-  //   component: LoginPage, 
-  //   meta: { layout:  LoginLayout, requiresVisitor: true } 
   // },
   // { 
   //   path: routePath.forgotPassword.path, 
