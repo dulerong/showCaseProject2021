8d8b5c48db136b2c6bbbcdf1f91d82118443bca3
Le-Jung Du <lejung@qualias.jp>
2021-07-10 11:29:07 +0900

    stat page

---
+++ resources/js/vue/pages/StatPage.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+div
+  v-form(v-model="isValid")
+    v-row(no-gutters justify="space-around")
+      div(style="width: 200px")
+        v-select(label="Site" :items="sites" v-model="siteSelected" :rules="rulesSelect" item-text="name" item-value="value" multiple chips clearable)
+      div(style="width: 200px")
+        v-select(label="Mother Age" :items="motherAges" v-model="motherAgeSelected" :rules="rulesSelect" clearable)
+      div(style="width: 200px")
+        v-select(label="Birth Order" :items="birthOrders" v-model="birthOrderSelected" :rules="rulesSelect" clearable)
+      div(style="width: 200px") 
+        v-select(label="Birth Sex" :items="sex" v-model="sexSelected" :rules="rulesSelect" clearable)
+    v-row(no-gutters)
+      //- v-btn(class="mr-1" @click="fetchData" :disabled="!isValid" ) Fetch Data
+      v-btn(class="mr-1" @click="resetData" color="info") Reset Data
+      v-btn(class="ml-1" @click="clearData" color="warning") Clear Data
+  v-data-table(:headers="headers" :items="data")
+    template(v-slot:item.birth_count="{ item }")
+      v-chip(:color="getColor(item.birth_count)" dark) {{ item.birth_count }}
+</template>
+
+<script>
+import mockData from '~js/mockData/stat'
+export default {
+  created() {
+    this.data = mockData.result.records
+  },
+  data: (vm) => ({
+    isValid: false,
+    sites: [
+      { value: '新北市板橋區', name: '板橋' },
+      { value: '新北市三重區', name: '三重' },
+      { value: '新北市中和區', name: '中和' },
+      { value: '新北市永和區', name: '永和' },
+      { value: '新北市新莊區', name: '新莊' }
+    ],
+    motherAges: [
+      '未滿15歲', '15～19歲', '20～24歲', '25～29歲', '30～34歲', '35～39歲', '40～44歲', '45～49歲', '50歲以上'
+    ],
+    birthOrders: [
+      '1胎', '2胎', '3胎', '4胎', '5胎', '6胎', '7胎', '8胎', '9胎以上'
+    ],
+    sex: [
+      '男', '女'
+    ],
+    sexSelected: null,
+    siteSelected: null,
+    motherAgeSelected: null,
+    birthOrderSelected: null,
+    rulesSelect: [],
+    headers: [
+      { text: 'Site', align: 'start', sortable: false, value: 'site_id', },
+      { text: 'Birth Order', value: 'birth_order' },
+      { text: 'Birth Sex', value: 'birth_sex' },
+      { text: 'Mother Age', value: 'mother_age' },
+      { text: 'Birth Count', value: 'birth_count' },
+    ],
+    data: [],
+  }),
+  computed: {
+    filterSelected() {
+      return `${this.siteSelected}|${this.motherAgeSelected}|${this.birthOrderSelected}|${this.sexSelected}`
+    }
+  },
+  watch: {
+    filterSelected: function () {
+      this.fetchData()
+    }
+  },
+  methods: {
+    fetchData() {
+      this.data = mockData.result.records
+        .filter(item => this.siteSelected ? this.siteSelected.includes(item.site_id) : true)
+        .filter(item => this.birthOrderSelected ? item.birth_order === this.birthOrderSelected : true)
+        .filter(item => this.motherAgeSelected ? item.mother_age === this.motherAgeSelected : true)
+        .filter(item => this.sexSelected ? item.birth_sex === this.sexSelected : true)
+    },
+    clearData() {
+      this.data = []
+    },
+    resetData() {
+      this.data = mockData.result.records
+    },
+    getColor(count) {
+      return count > 0 ? 'red' : ''
+    }
+  }
+}
+</script>
