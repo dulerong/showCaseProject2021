97efa9600d6ec478802200db6fd67be7fb80300d
dulerong <codeydu@hotmail.com>
2021-07-19 12:14:46 +0900

    integrated toggle side drawer

--- resources/js/vue/layouts/default.vue
+++ resources/js/vue/layouts/default.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
 v-app
-  AppBar(v-bind='drawer')
+  AppBar(v-bind='drawer' :toggleSide='sideDrawer.toggle')
   NavigationDrawer(:drawer='drawer')
+  SettingDrawer(:sideDrawer='sideDrawer')
   v-main
     div(class="pa-4 mx-auto" style="max-width: 1280px; height: 100%")
       slot
@@ -14,6 +15,7 @@ export default {
   components: {
     AppBar: require('~components/AppBar').default,
     NavigationDrawer: require('~components/NavigationDrawer').default,
+    SettingDrawer: require('~components/SettingDrawer').default,
     SnackBar: require('~components/SnackBar').default,
     Footer: require('~components/Footer').default,
   },
@@ -21,11 +23,18 @@ export default {
     drawer: {
       isMini: false,
       toggle: vm.toggleDrawer
+    },
+    sideDrawer: {
+      isMini: false,
+      toggle: vm.toggleSideDrawer
     }
   }),
   methods: {
     toggleDrawer() {
       this.drawer.isMini = !this.drawer.isMini
+    },
+    toggleSideDrawer() {
+      this.sideDrawer.isMini = !this.sideDrawer.isMini
     }
   }
 }
