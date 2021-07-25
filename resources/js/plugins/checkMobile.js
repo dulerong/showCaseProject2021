18d7561def4104a065aa2b8659a01fe726eb1def
dulerong <codeydu@hotmail.com>
2021-07-25 13:59:48 +0900

    userAgentData migrate

--- resources/js/plugins/checkMobile.js
+++ resources/js/plugins/checkMobile.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 
-const MOBILE_REGEX = [
-  /Android/i,
-  /webOS/i,
-  /iPhone/i,
-  /iPad/i,
-  /iPod/i,
-  /BlackBerry/i,
-  /Windows Phone/i
-]
+const USER_AGENT_DATA_MOBILE = navigator.userAgentData.mobile
 
-const IS_MOBILE = () => MOBILE_REGEX.some(item => navigator.userAgent.match(item))
+// const MOBILE_REGEX = [
+//   /Android/i,
+//   /webOS/i,
+//   /iPhone/i,
+//   /iPad/i,
+//   /iPod/i,
+//   /BlackBerry/i,
+//   /Windows Phone/i
+// ]
 
-const IS_TOUCH_SCREEN = () => navigator.maxTouchPoints > 0 || IS_MOBILE()
+// const IS_MOBILE = () => MOBILE_REGEX.some(item => navigator.userAgent.match(item))
+
+const IS_TOUCH_SCREEN = () => navigator.maxTouchPoints > 0 || USER_AGENT_DATA_MOBILE
 // iOS 12 or below has no navigator.maxTouchPoints, hence need to check userAgent
 
 Vue.prototype.$_IS_TOUCH_SCREEN = IS_TOUCH_SCREEN()
