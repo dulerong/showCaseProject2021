7a55e8db5a86138623818d9b73033377940ef0ea
dulerong <codeydu@hotmail.com>
2021-07-25 13:49:58 +0900

    implented i18n

--- resources/js/vue/components/Footer.vue
+++ resources/js/vue/components/Footer.vue
@@ -4,7 +4,7 @@ v-footer(padless absolute app)
     v-card-text
       v-row(no-gutters justify="center")
         v-btn(v-for="(icon, i) in icons" :key="i" icon class="mx-4")
-          ToolTip(:message="$_language[$route.params.lang].footer.tooltip[icon.hint]")
+          ToolTip(:message="$t(`footer.tooltip.${icon.hint}`)")
             v-icon {{ icon.name }}
     v-divider
     v-card-text
