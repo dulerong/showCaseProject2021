b9bef1097a3fc325e784e4b745f02a0b5ef18a48
dulerong <codeydu@hotmail.com>
2021-07-26 11:38:58 +0900

    menu language component

---
+++ resources/js/vue/components/MenuLanguage.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+v-menu(open-on-hover transition="slide-y-transition" offset-y)
+  template(v-slot:activator="{ on, attrs }")
+    v-btn(text class="pa-0" v-bind="attrs" v-on="on")
+      v-icon mdi-translate
+      v-icon(small) mdi-chevron-down
+  v-card(class="px-2")
+    v-list
+      v-list-item
+        v-list-item-content(class="font-weight-bold") {{ $t('appBar.translate') }}
+      v-list-item(v-for="(item, i) in listLanguages" :key="item.value" link :to="{ params: { lang: item.value }}")
+        v-list-item-content {{ item.title }}
+</template>
+
+<script>
+export default {
+  data: () => ({
+    listLanguages: [
+      { title: 'English', value: 'en' },
+      { title: '中文', value: 'ch' },
+    ]
+  })
+}
+</script>
