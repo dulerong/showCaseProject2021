ab271af7cf4de65a1bc2d3ccc536566261a5ce81
dulerong <codeydu@hotmail.com>
2021-08-08 20:58:40 +0900

    overhauled population page, included chart component

--- resources/js/vue/pages/JapanPopulationPage.vue
+++ resources/js/vue/pages/JapanPopulationPage.vue
@@ -1,39 +1,52 @@
 <template lang="pug">
 div
-  h1 Japan Population Page
+  h1 {{ $t("japanPopulation.title") }}
   v-row(no-gutters)
-    v-select(label="prefectures" :items="prefectureList" v-model="prefectureSelected" item-text="name" item-value="code" return-object multiple chips clearable)
+    v-select(label="prefectures" :items="$_prefectures" v-model="prefectureSelected" item-text="name" item-value="code" return-object multiple chips clearable)
+  Chart(:chartData="chartData")
 </template>
 
 <script>
 import axios from 'axios'
 import { mapMutations } from 'vuex'
 export default {
-  data: (vm) => ({
-    prefectureList: vm.$_prefectures,
+  components: {
+    Chart: require('~components/Chart').default
+  },
+  data: () => ({
     prefectureSelected: [],
-    data: [],
     error: false,
     apiResponse: null,
+    chartData: []
   }),
   watch: {
-    prefectureSelected: function (val, oldVal) {
-      const DIFF = val.find(item => !oldVal.includes(item))
-      this.fetchData(DIFF.code)
-    }
+    prefectureSelected: async function (val, oldVal) {
+      if (val.length === 0) {
+        this.chartData = []
+      } else if (val.length < oldVal.length) {
+        const deletedPrefecture = oldVal.find(item => !val.includes(item))
+        this.chartData = this.chartData.filter(item => item.name !== deletedPrefecture.name)
+      } else {
+        const newPrefecture = val.find(item => !oldVal.includes(item))
+        await this.fetchData(newPrefecture)
+        
+        if (this.error) return
+        this.chartData.push({ name: newPrefecture.name, data: this.apiResponse})
+      }
+    },
   },
   methods: {
     ...mapMutations('notification', ['showNotification']),
-    fetchData(prefCode) {
+    fetchData: async function(prefecture) {
       const API_ADDRESS = "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear"
-      const params = { prefCode }
+      const params = { prefCode: prefecture.code }
       const API_KEY = process.env.NODE_ENV === 'test' ? 'API_KEY' : process.env.MIX_RESAS_KEY
       const headers = {
         "Content-Type": "application/json",
         "X-API-KEY": API_KEY
       }
-      axios.get(API_ADDRESS, { params, headers })
-        .then(res => this.apiResponse = res.data.result.data[0].data)
+      return axios.get(API_ADDRESS, { params, headers })
+        .then(res => this.apiResponse = res.data.result.data[0].data.map(item => item.value))
         .catch(() => {
           this.error = true
           this.showNotification({ message: 'Error occurred fetching data', color: 'error' })
