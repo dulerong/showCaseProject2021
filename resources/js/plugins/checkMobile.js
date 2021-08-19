3153d78c9a3b6494da00795938074f7d0d0c17c5
dulerong <codeydu@hotmail.com>
2021-08-19 18:17:57 +0900

    added check dark mode

--- resources/js/plugins/checkMobile.js
+++ resources/js/plugins/checkMobile.js
@@ -18,3 +18,8 @@ const IS_TOUCH_SCREEN = () => navigator.maxTouchPoints > 0 || IS_MOBILE()
 // iOS 12 or below has no navigator.maxTouchPoints, hence need to check userAgent
 
 Vue.prototype.$_IS_TOUCH_SCREEN = IS_TOUCH_SCREEN()
+
+// dark-mode media query matched or not
+const IS_DARK = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+Vue.prototype.$_IS_DARK = IS_DARK
