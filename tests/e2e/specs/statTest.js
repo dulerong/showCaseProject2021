838c65f3666b98758a33a7997c25ddb73e2308ef
dulerong <codeydu@hotmail.com>
2021-08-26 12:01:57 +0900

    e2e test for stat page

---
+++ tests/e2e/specs/statTest.js
@@ -0,0 +1,20 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('Japan Population Page tests', () => {
+  const visit = () => cy.visit('http://127.0.0.1:8000/en/stat')
+
+  beforeEach(() => {
+    visit()
+  })
+
+  it('User select districts and obtain data', () => {
+    cy.window().its('StatPage.data').should('have.length', 0)
+    cy.get('[data-testid="statPageSelectDistrict"]').click({ force: true })
+
+    cy.contains('Banqiao').click()
+    cy.get('h1:first').click()
+    cy.contains('FETCH DATA').click()
+
+    cy.window().its('StatPage.data').should('not.have.length', 0)
+  })
+});
