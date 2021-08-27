ca60f2b49251b9bb0d66619048c70a381dad8d27
dulerong <codeydu@hotmail.com>
2021-08-27 12:45:01 +0900

    abstracted v-select

---
+++ resources/js/vue/components/VSelectMulti.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+v-select(
+  :label="label"
+  :items="items"
+  v-model="selected"
+  item-text="name"
+  item-value="value"
+  multiple
+  chips
+  clearable
+  :data-testid="dataTestID"
+)
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      required: true,
+      type: String,
+    },
+    items: {
+      required: true,
+      type: Array,
+    },
+    dataTestID: {
+      required: false,
+      type: String,
+      default: ''
+    }
+  },
+  data: () => ({
+    selected: null,
+  }),
+  watch: {
+    selected: function(val) {
+      this.$emit('selected-data', val)
+    }
+  },
+}
+</script>
